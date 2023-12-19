83e6e68f6ad7b2804d120bd57a4d3dd840fd45a5
Leftwind <andreselfisico@gmail.com>
2023-12-19 16:17:14 +0100

    New Main Window

--- main_app.py
+++ main_app.py
@@ -1,5 +1,6 @@
 import sys 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, \
+QToolBar, QVBoxLayout, QTextEdit
 
 
 class MainWindow(QMainWindow):
@@ -9,5 +10,49 @@ class MainWindow(QMainWindow):
         self.init_ui()
 
     def init_ui(self):
-        self.setWindwoTitle("Spacy Text Processor")
-        self.setBaseSize()
+        self.setWindowTitle("Spacy Text Processor")
+        self.setMinimumSize(800, 800)
+
+        #Create Central Widget: 
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+        search_menu_item = self.menuBar().addMenu("&Search")
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+
+        self.layout = QVBoxLayout()
+
+        self.original_text_edit = QTextEdit()
+        self.layout.addWidget(self.original_text_edit)
+
+        self.extract_button = QPushButton("Extract Keywords")
+        self.extract_button.clicked.connect(self.extract_keywords)
+        self.layout.addWidget(self.extract_button)
+
+        self.keywords_text_edit = QTextEdit()
+        self.keywords_text_edit.setReadOnly(True)  # Make it read-only
+        self.layout.addWidget(self.keywords_text_edit)
+
+        self.central_widget.setLayout(self.layout)
+
+    def extract_keywords():
+        pass    
+
+
+
+
+
+
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
