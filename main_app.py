d14dd15e42a9eca6b8ed1cd87f5228d7b9e133b7
Leftwind <andreselfisico@gmail.com>
2023-12-20 13:42:33 +0100

    UPDATED

--- main_app.py
+++ main_app.py
@@ -1,8 +1,11 @@
+import os
 import sys 
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, \
 QToolBar, QVBoxLayout, QTextEdit, QFileDialog
 from PyQt6.QtGui import QAction, QIcon
 import spacy
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -24,6 +27,11 @@ class MainWindow(QMainWindow):
         upload_file_action = QAction(QIcon("icons/upload_file.png"), "Upload File", self)
         upload_file_action.triggered.connect(self.upload)
         file_menu_item.addAction(upload_file_action)
+
+        #Clean HTML and XML
+        clean_file_action = QAction(QIcon("icons/clean.png"), "Clean HTML/XML", self)
+        clean_file_action.triggered.connect(self.clean_file)
+        file_menu_item.addAction(clean_file_action)
 
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
@@ -54,6 +62,42 @@ class MainWindow(QMainWindow):
             with open(file_path, encoding="utf8") as file: 
                 file_contents = file.read()
                 self.original_text_edit.setPlainText(file_contents)
+                return file_path
+
+    def clean_file(self):
+        if not hasattr(self, 'file_path'):
+            self.upload()  # If file_path attribute is not set, trigger file upload
+
+        if not self.file_path:
+            return
+
+        file_extension = os.path.splitext(self.file_path)[1].lower()
+
+        if file_extension == '.xml':
+            cleaned_text = self.clean_xml_file(self.file_path)
+        elif file_extension in ['.html', '.htm']:
+            cleaned_text = self.clean_html_file(self.file_path)
+        else:
+            self.keywords_text_edit.setPlainText("Unsupported file format.")
+            return
+
+        self.original_text_edit.setPlainText(cleaned_text)
+
+    def clean_html_file(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            html_content = file.read()
+
+        soup = BeautifulSoup(html_content, 'html.parser')
+        cleaned_text = soup.get_text(separator='\n', strip=True)
+        return cleaned_text
+
+    def clean_xml_file(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            xml_content = file.read()
+
+        root = ET.fromstring(xml_content)
+        cleaned_text = ' '.join(root.itertext())
+        return cleaned_text
 
     def extract_keywords(self):
         text = self.original_text_edit.toPlainText()
