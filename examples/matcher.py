78b848f53fed12ec4fe0a93b2de83c2da3090e41
Leftwind <andreselfisico@gmail.com>
2023-12-27 11:28:40 +0100

    Matcher prototype

---
+++ examples/matcher.py
@@ -0,0 +1,16 @@
+import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_sm")
+matcher = Matcher(nlp.vocab)
+
+#Add Pattern
+pattern = [{"LOWER": "hello"}, {"IS_PUNCT": True}, {"LOWER": "world"}]
+matcher.add("HelloWorld", [pattern])
+
+doc = nlp("Hello, world! Hello world!")
+matches = matcher(doc)
+for match_id, start, end in matches:
+    string_id = nlp.vocab.strings[match_id]  # Get string representation
+    span = doc[start:end]  # The matched span
+    print(match_id, string_id, start, end, span.text)
